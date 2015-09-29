b7f39b999700f7cdc75697aadd0de2ca8ed705e0
Nat Wilson <njwilson23@gmail.com>
2015-09-28 22:54:24 -0400

    hacks to avoid segfault during tape playback

--- adjoint/ad_rev_model.f90
+++ adjoint/ad_rev_model.f90
@@ -6,41 +6,37 @@ program driver
     implicit none
 
     ! Run workshop example
-    integer, parameter              :: len = 50
-    double precision, parameter     :: dx = 1000.0, dt = 0.05, end_time = 1000.0
+    integer, parameter              :: sz = 50
+    double precision                :: dx, dt, end_time
 
-    double precision, dimension(len):: x    ! horizontal coordinate
-    double precision, dimension(len):: b    ! base elevation
-    type(active), dimension(len)    :: s, mb
+    double precision, dimension(sz) :: x    ! horizontal coordinate
+    double precision, dimension(sz) :: b    ! base elevation
+    type(active), dimension(sz)     :: s, mb
     type(active)                    :: volume
 
-    integer                         :: i
+    integer                         :: i, sz_
 
-    do i = 1, len
+    dx = 1000.0
+    dt = 0.05
+    end_time = 1000.0
+    sz_ = sz
+
+    our_rev_mode%tape = .true.
+
+    do i = 1, sz
         x(i) = (i-1)*dx
         b(i) = -2.5e-2 * x(i)
         s(i)%v = b(i)
         s(i)%d = 0.0
         mb(i)%v = 4.0 - 0.2e-3 * x(i)
-        mb(i)%d = 0.0
     end do
 
     volume%d=1.0
 
-    !our_rev_mode%tape = .true.
-    !our_rev_mode%store = .false.
-    !call tape_init()
+    call integrate_model(sz_, x, b, s, mb, volume, dt, end_time)
 
-    ! Record the forward model
-    !call OAD_revTape()
-    call integrate_model(x, b, s, mb, volume, dt, end_time)
-
-    ! Back out the adjoint (why do I call the same model function as above?)
-    !call OAD_revAdjoint()
-    !call integrate_model(x, b, s, mb, volume, dt, end_time)
-
-    do i = 1, len
-        print*, mb
+    do i = 1, sz
+        print*, mb(i)
     end do
 
 end program
--- adjoint/kees_model.f90
+++ adjoint/kees_model.f90
@@ -11,28 +11,30 @@ module kees_model
 contains
 
     ! WARNING: modifies s IN-PLACE
-    subroutine integrate_model(x, b, s, mb, volume, dt, end_time)
+    subroutine integrate_model(sz, x, b, s, mb, volume, dt, end_time)
 
         implicit none
 
-        double precision, dimension(:), intent(inout)   :: mb
-        double precision, dimension(:), intent(inout)   :: x    ! horizontal coordinate
-        double precision, dimension(:), intent(inout)   :: b    ! base elevation
-        double precision, dimension(:), intent(inout)   :: s    ! surface elevation
+        integer                                         :: sz
+        double precision, dimension(sz), intent(inout)  :: mb
+        double precision, dimension(sz), intent(inout)  :: x    ! horizontal coordinate
+        double precision, dimension(sz), intent(inout)  :: b    ! base elevation
+        double precision, dimension(sz), intent(inout)  :: s    ! surface elevation
         double precision, intent(inout)                 :: volume
         double precision, intent(inout)                 :: dt, end_time
+
         double precision                    :: time
         integer                             :: i
 
         !$openad INDEPENDENT(mb)
 
         time = 0.0
-
         do while (time .lt. end_time)
-            call timestep_explicit(x, b, s, dt, mb)
+            call timestep_explicit(sz, x, b, s, dt, mb)
+            time = time + dt
         end do
 
-        volume = glacier_volume(x, b, s)
+        volume = glacier_volume(sz, x, b, s)
 
         !$openad DEPENDENT(volume)
 
@@ -40,11 +42,12 @@ contains
 
     ! Compute diffusivity for a model instance for a particular glen
     ! exponent
-    subroutine diffusivity(x, b, s, n, nu)
+    subroutine diffusivity(sz, x, b, s, n, nu)
         implicit none
-        double precision, dimension(:), intent(inout)   :: x, b, s
+        integer, intent(inout)                          :: sz
+        double precision, dimension(sz), intent(inout)  :: x, b, s
         integer, intent(inout)                          :: n
-        double precision, dimension(size(x)-1), intent(inout)   :: nu
+        double precision, dimension(sz-1), intent(inout):: nu
         integer                                         :: i
 
         do i = 1, size(nu)
@@ -54,66 +57,61 @@ contains
     end subroutine
 
     ! Return the glacier volume (trapezoid rule)
-    double precision function glacier_volume(x, b, s) result(sigma)
+    double precision function glacier_volume(sz, x, b, s) result(sigma)
 
         implicit none
-        double precision, dimension(:), intent(inout)   :: x, b, s
+        integer, intent(inout)                          :: sz
+        double precision, dimension(sz), intent(inout)  :: x, b, s
         integer                                         :: i
 
-        do i = 1, size(x)-1
-            sigma = sigma + &
-                (x(i+1) - x(i)) * 0.5 * (s(i+1) +s(i) - b(i+1) - b(i))
+        sigma = 0.0
+        do i = 1, sz-1
+            sigma = sigma + (x(i+1) - x(i)) * 0.5 * (s(i+1) +s(i) - b(i+1) - b(i))
         end do
     end function
 
     ! Perform an explicit finite difference forward step for a model
     ! instance and return a new model instance
-    subroutine timestep_explicit(x, b, s, dt, mb)
+    subroutine timestep_explicit(sz, x, b, s, dt, mb)
 
         implicit none
 
-        double precision, dimension(:), intent(inout)   :: x, b, mb
-        double precision, dimension(:), intent(inout)   :: s
+        integer, intent(inout)                          :: sz
+        double precision, dimension(sz), intent(inout)  :: x, b, mb
+        double precision, dimension(sz), intent(inout)  :: s
         double precision, intent(inout)                 :: dt
 
-        integer                                         :: len, i, n
-        double precision, dimension(:), allocatable     :: nu
-        double precision, dimension(:), allocatable     :: flux
-        double precision, dimension(:), allocatable     :: dHdt
+        integer                                         :: i, n
+        double precision, dimension(sz-1)               :: nu, flux
+        double precision, dimension(sz)                 :: dHdt
 
         n = 3
-        len = size(s)
-        allocate(nu(len-1), flux(len-1), dHdt(len))
-
-        call diffusivity(x, b, s, n, nu)
-        do i = 1, len-1
-            flux(i) = -nu(i) * &
-                (s(i+1) - s(i)) / (x(i+1) - x(i))
+        call diffusivity(sz, x, b, s, n, nu)
+        do i = 1, sz-1
+            flux(i) = -nu(i) * (s(i+1) - s(i)) / (x(i+1) - x(i))
         end do
 
         dHdt(1) = -mb(1)
-        dHdt(len) = -mb(len)
-        do i = 2, len-1
+        dHdt(sz) = -mb(sz)
+        do i = 2, sz-1
             dHdt(i) = -(flux(i) - flux(i-1)) / (x(i) - x(i-1))
         end do
 
         ! Apply mass balance
-        do i = 1, len
-            if (s(i) + dt*(dHdt(i)+mb(i)) .lt. b(i)) then
+        do i = 1, sz
+            s(i) = s(i) + dt*(dHdt(i)+mb(i))
+            if (s(i) .lt. b(i)) then
                 s(i) = b(i)
-            else
-                s(i) = s(i) + dt*(dHdt(i)+mb(i))
-            end if
+            endif
         end do
-
-        deallocate(nu, flux, dHdt)
 
     end subroutine
 
-    subroutine writemodel(filename, x, b, s)
+    subroutine writemodel(filename, sz, x, b, s)
 
         character(len=8), intent(inout)                 :: filename
-        double precision, dimension(:), intent(inout)   :: x, b, s
+        integer                                         :: sz
+        double precision, dimension(sz), intent(inout)  :: x, b, s
         integer                                         :: ios
 
         open(unit=20, file=filename, form="formatted", status="old", iostat=ios)
